f9e970449523b4709de4f80c67eff61e8a203b6e
airoceanic <airoceanic.org@gmail.com>
2026-06-06 11:27:50 +1000

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,63 +1,102 @@
 <?php if (enable_cookie_banner) {?>
 <div class="cookie-banner text-center" style="display: none">
     <p>
-        This site uses cookies. By continuing to browse this site, you accept the use of cookies. <button
-            id="cookieAccept" class="btn btn-cta-primary">Ok</button>
+        This site uses cookies. By continuing to browse this site, you accept the use of cookies.
+        <button id="cookieAccept" class="btn btn-cta-primary">Ok</button>
     </p>
 </div>
 <?php } ?>
+
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-md-8">
-                <small class="copyright">Copyright &copy; <?php echo date("Y");?>
-                    Air Oceanic</small><br />
-                <small><a
-                        href="<?php echo website_base_url;?>privacy.php">Privacy
-                        Policy</a> | <a
-                        href="<?php echo website_base_url;?>staff.php">Contact
-                        Us</a></small>
-             </div>
+
+            <!-- VATSIM Section -->
+            <div class="col-md-4 text-center">
+                <a href="https://www.vatsim.net" target="_blank">
+                    <img src="<?php echo website_base_url; ?>assets/images/vatsim-logo.png"
+                         alt="VATSIM"
+                         style="max-width:250px;width:100%;height:auto;margin-bottom:15px;">
+                </a>
+
+                <small>
+                    As a VATSIM Virtual Airline Partner, Air Oceanic mandates our pilots
+                    to utilise the VATSIM network when flying to enhance immersion and realism.
+                </small>
+            </div>
+
+            <!-- Copyright -->
+            <div class="col-md-4 text-center">
+                <small class="copyright">
+                    Copyright &copy; <?php echo date("Y"); ?> Air Oceanic
+                </small>
+                <br />
+                <small>
+                    <a href="<?php echo website_base_url; ?>privacy.php">Privacy Policy</a>
+                    |
+                    <a href="<?php echo website_base_url; ?>staff.php">Contact Us</a>
+                </small>
+            </div>
+
+            <!-- Social Icons -->
             <div class="col-md-4 text-right">
                 <?php if (!empty(facebook_footer_link)) {?>
                 <a href="<?php echo facebook_footer_link?>"
-                    class="social-icons" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+                    class="social-icons" target="_blank">
+                    <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                </a>
                 <?php } ?>
+
                 <?php if (!empty(twitter_footer_link)) {?>
                 <a href="<?php echo twitter_footer_link?>"
-                    class="social-icons" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+                    class="social-icons" target="_blank">
+                    <i class="fa fa-twitter-square" aria-hidden="true"></i>
+                </a>
                 <?php } ?>
+
                 <?php if (!empty(youtube_footer_link)) {?>
                 <a href="<?php echo youtube_footer_link?>"
-                    class="social-icons" target="_blank"><i class="fa fa-brands fa-youtube" aria-hidden="true"></i></a>
+                    class="social-icons" target="_blank">
+                    <i class="fa fa-brands fa-youtube" aria-hidden="true"></i>
+                </a>
                 <?php } ?>
+
                 <?php if (!empty(discord_footer_link)) {?>
                 <a href="<?php echo discord_footer_link?>"
-                    class="social-icons" target="_blank"><i class="fa fa-brands fa-discord" aria-hidden="true"></i></a>
+                    class="social-icons" target="_blank">
+                    <i class="fa fa-brands fa-discord" aria-hidden="true"></i>
+                </a>
                 <?php } ?>
+
                 <?php if (!empty(instagram_footer_link)) {?>
                 <a href="<?php echo instagram_footer_link?>"
-                    class="social-icons" target="_blank"><i class="fa fa-brands fa-instagram"
-                        aria-hidden="true"></i></a>
+                    class="social-icons" target="_blank">
+                    <i class="fa fa-brands fa-instagram" aria-hidden="true"></i>
+                </a>
                 <?php } ?>
             </div>
+
         </div>
     </div>
 </footer>
+
 <script type="text/javascript"
     src="<?php echo website_base_url;?>assets/plugins/bootstrap/js/bootstrap.min.js">
 </script>
+
 <script
     src="<?php echo website_base_url;?>assets/plugins/jquery.waypoints.min.js">
 </script>
+
 <script type="text/javascript"
     src="<?php echo website_base_url;?>assets/plugins/jquery.counterup.min.js">
 </script>
 
 <script type="text/javascript"
     src="<?php echo website_base_url;?>assets/js/main.js"></script>
+
 <script type="text/javascript"
     src="<?php echo website_base_url;?>assets/js/loader.js"></script>
-</body>
 
+</body>
 </html>
